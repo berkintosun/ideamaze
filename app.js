2054fb5a405f7d13a18f3745c1f5163ab128ec73
Berkin Tosun <berkintsn@gmail.com>
2019-07-19 16:26:08 +0300

    Initial setup of handlebars and basic routing

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const app = express();
 
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars',exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine','handlebars');
+
+//Routes
+//Index
+app.get('/', (req,res) => {
+    const Param = "Index"
+    res.render("index",{
+        Param:Param
+    });
+});
+
+//About
+app.get('/about',(req,res) => {
+    res.render("about");
+})
+
 const port = 5000;
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
